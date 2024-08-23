e03762c49745204f2071d7cca7ab38051b511620
snehajoshi90 <snehalvaidya19@gmail.com>
2024-08-23 11:08:32 +0530

    product details screen commit

--- lib/screens/product_details/product_detail_screen.dart
+++ lib/screens/product_details/product_detail_screen.dart
@@ -145,40 +145,38 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
                               fontWeight: FontWeight.w700, fontSize: 16),
                         ),
                         const Spacer(),
-
-
-                    RatingStars(
-                      value: value,
-                      onValueChanged: (v) {
-                        //
-                        setState(() {
-                          value = v;
-                        });
-                      },
-                      starBuilder: (index, color) => Icon(
-                        Icons.star,
-                        color: color,
-                      ),
-                      starCount: 5,
-                      starSize: 20,
-                      valueLabelColor: const Color(0xff9b9b9b),
-                      valueLabelTextStyle: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w400,
-                          fontStyle: FontStyle.normal,
-                          fontSize: 12.0),
-                      valueLabelRadius: 10,
-                      maxValue: 5,
-                      starSpacing: 2,
-                      maxValueVisibility: true,
-                      valueLabelVisibility: false,
-                      //  animationDuration: Duration(milliseconds: 1000),
-                      valueLabelPadding: const EdgeInsets.symmetric(
-                          vertical: 1, horizontal: 8),
-                      valueLabelMargin: const EdgeInsets.only(right: 8),
-                      starOffColor: const Color(0xffe7e8ea),
-                      starColor: Color(0xffF3603F),
-                    ),
+                        RatingStars(
+                          value: value,
+                          onValueChanged: (v) {
+                            //
+                            setState(() {
+                              value = v;
+                            });
+                          },
+                          starBuilder: (index, color) => Icon(
+                            Icons.star,
+                            color: color,
+                          ),
+                          starCount: 5,
+                          starSize: 20,
+                          valueLabelColor: const Color(0xff9b9b9b),
+                          valueLabelTextStyle: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w400,
+                              fontStyle: FontStyle.normal,
+                              fontSize: 12.0),
+                          valueLabelRadius: 10,
+                          maxValue: 5,
+                          starSpacing: 2,
+                          maxValueVisibility: true,
+                          valueLabelVisibility: false,
+                          //  animationDuration: Duration(milliseconds: 1000),
+                          valueLabelPadding: const EdgeInsets.symmetric(
+                              vertical: 1, horizontal: 8),
+                          valueLabelMargin: const EdgeInsets.only(right: 8),
+                          starOffColor: const Color(0xffe7e8ea),
+                          starColor: Color(0xffF3603F),
+                        ),
                         IconButton(
                             onPressed: () {},
                             icon: Icon(
@@ -187,7 +185,9 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
                             )),
                       ],
                     ),
-                    SizedBox(height: 60,)
+                    SizedBox(
+                      height: 60,
+                    )
                   ],
                 ),
               ),
@@ -201,7 +201,8 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
                 fixedSize: Size(MediaQuery.of(context).size.width, 49),
                 foregroundColor: Colors.white,
                 backgroundColor: Color(0xff53B175),
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(19)),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(19)),
               ),
               onPressed: () {},
               child: CustomText(
